faa2ce9306b11a4b81c7aedba4624b459a060d3b
tankwanghow <tankwanghow@gmail.com>
2015-03-10 18:37:34 +0800

    backup commit

---
+++ app/models/sales_order_detail.rb
@@ -0,0 +1,37 @@
+class SalesOrderDetail < ActiveRecord::Base
+  belongs_to :sales_order
+  belongs_to :product
+  belongs_to :product_packaging
+  
+  validates_presence_of :product_name1, :unit
+  validates_numericality_of :quantity, greater_than: 0
+  
+  include ValidateBelongsTo
+  validate_belongs_to :product, :name1
+  
+  def simple_audit_string
+    [ product.name1, quantity, unit_price, discount ].join ' '
+  end
+
+  def unit
+    product.unit if product
+  end
+
+  def packaging_name
+    product_packaging.pack_qty_name if product_packaging
+  end
+
+  def packaging_name= val
+    if !val.blank?
+      pid = ProductPackaging.find_product_package(product_id, val).try(:id)
+      if pid
+        self.product_packaging_id = pid
+      else
+        errors.add 'packaging_name', 'not found!'
+      end
+    else
+      self.product_packaging_id = nil
+    end
+  end
+
+end
