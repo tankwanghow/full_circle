afbc7d7cba6f8d8931d4d3f6bcc93c6bc7b1cc23
tankwanghow <tankwanghow@gmail.com>
2015-03-14 18:05:47 +0800

    changed to orders details

--- app/models/arrangement.rb
+++ app/models/arrangement.rb
@@ -1,22 +1,8 @@
 class Arrangement < ActiveRecord::Base
-  belongs_to :sales_order
-  belongs_to :purchase_order
-  belongs_to :loading_order
-  belongs_to :invoice
-  belongs_to :pur_invoice
+  belongs_to :sales_order_detail
+  belongs_to :purchase_order_detail
+  belongs_to :loading_order_detail
+  belongs_to :invoice_detail
+  belongs_to :pur_invoice_detail
 
-  scope :empty_with_sales_order, ->(val) { where(sales_order_id: val, purchase_order_id: nil, loading_order_id: nil, invoice_id: nil, pur_invoice_id: nil, quantity: 0) }
-
-  def self.create_with_sales_orders! ids
-    rtn = []
-    ids.each do |id|
-      arrangement = empty_with_sales_order(id.to_i).first
-      if arrangement
-        rtn << arrangement
-      else
-        rtn << create!(sales_order_id: id.to_i)
-      end
-    end
-    rtn
-  end
 end
