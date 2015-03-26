53e7d3300132ce7ff51bbe05d111e5b92b9b2e3f
tankwanghow <tankwanghow@gmail.com>
2015-03-26 17:57:21 +0800

    sync commit

--- db/migrate/20150212040054_create_loading_orders.rb
+++ db/migrate/20150212040054_create_loading_orders.rb
@@ -5,18 +5,9 @@ class CreateLoadingOrders < ActiveRecord::Migration
       t.belongs_to :transporter
       t.string     :lorry_no
       t.text       :note
+      t.boolean    :cancelled,    default: false
       t.integer    :lock_version, default: 0
       t.timestamps
-    end
-
-    create_table :loading_order_detail do |t|
-      t.belongs_to :sales_order_detail
-      t.belongs_to :purchase_order_detail
-      t.date       :loaded_at
-      t.decimal    :load_quantity, precision: 12, scale: 4, default: 0
-      t.date       :deliver_at
-      t.decimal    :deliver_quantity, precision: 12, scale: 4, default: 0
-      t.text       :note
     end
   end
 end
--- db/migrate/20150212040423_create_arrangements.rb
+++ db/migrate/20150212040423_create_arrangements.rb
@@ -3,10 +3,16 @@ class CreateArrangements < ActiveRecord::Migration
     create_table :arrangements do |t|
       t.belongs_to :sales_order_detail
       t.belongs_to :purchase_order_detail
-      t.belongs_to :loading_order_detail
+      t.belongs_to :loading_order
+      t.string     :note
+      t.date       :loaded_date
+      t.decimal    :load_quantity, precision: 12, scale: 4, default: 0
+      t.date       :deliver_date
+      t.decimal    :deliver_quantity, precision: 12, scale: 4, default: 0
+      t.boolean    :canceled, default: false
       t.belongs_to :invoice_detail
       t.belongs_to :pur_invoice_detail
-      t.integer :lock_version, default: 0
+      t.integer    :lock_version, default: 0
       t.timestamps
     end
   end
