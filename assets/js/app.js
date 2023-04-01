a15467c13bc0777f3b2bc687aa87fee897622910
Tan Kwang How <tankwanghow@gmail.com>
2023-04-01 22:37:56 +0800

    added remove-el

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,8 +18,8 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import InfiniteScroll from "./infinite_scroll"
 
@@ -32,10 +32,16 @@ let liveSocket = new LiveSocket("/live", Socket, {
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
+window.addEventListener("phx:remove-el", params => {
+  elm = document.getElementById(params.detail.id);
+  elm.className += " opacity-0 transition-opacity duration-1000";
+  setTimeout(() => elm.remove(), 1200);
+}
+)
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 
