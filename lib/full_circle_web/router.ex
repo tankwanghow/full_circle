29a5e22c5d564c436d455c6e96fb4af071fb5d3c
Tan Kwang How <tankwanghow@gmail.com>
2023-07-14 10:40:09 +0800

    fix warning

--- lib/full_circle_web/router.ex
+++ lib/full_circle_web/router.ex
@@ -45,7 +45,7 @@ defmodule FullCircleWeb.Router do
       pipe_through(:browser)
 
       # live_dashboard("/dashboard", metrics: FullCircleWeb.Telemetry)
-      forward("/mailbox", Plug.Swoosh.MailboxPreview)
+      # forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 
