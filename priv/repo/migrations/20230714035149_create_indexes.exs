7d2978dadfd67bedc91d1a2587757765ef1fc2a3
Tan Kwang How <tankwanghow@gmail.com>
2023-07-18 15:21:25 +0800

    make index quicker

--- priv/repo/migrations/20230714035149_create_indexes.exs
+++ priv/repo/migrations/20230714035149_create_indexes.exs
@@ -2,14 +2,15 @@ defmodule FullCircle.Repo.Migrations.CreateIndexed do
   use Ecto.Migration
 
   def change do
-    create index(:transactions, [:doc_type, :doc_no])
+    create index(:transactions, [:doc_type, :doc_no, :company_id])
     create index(:transactions, [:company_id])
-    create index(:transactions, [:contact_id])
-    create index(:transactions, [:account_id])
-    create index(:transactions, [:fixed_asset_id])
-    create index(:contacts, [:name])
-    create index(:accounts, [:name])
-    create index(:goods, [:name])
-    create index(:fixed_assets, [:name])
+    create index(:transactions, [:contact_id, :company_id])
+    create index(:transactions, [:account_id, :company_id])
+    create index(:transactions, [:fixed_asset_id, :company_id])
+    create index(:contacts, [:name, :company_id])
+    create index(:accounts, [:name, :company_id])
+    create index(:goods, [:name, :company_id])
+    create index(:fixed_assets, [:name, :company_id])
+    create index(:tax_codes, [:code, :company_id])
   end
 end
