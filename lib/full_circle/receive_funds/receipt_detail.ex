0eb1fae824736287be84ebf4fb69b7798aef162f
Tan Kwang How <tankwanghow@gmail.com>
2023-08-10 12:11:18 +0800

    added receipt details

---
+++ lib/full_circle/receive_funds/receipt_detail.ex
@@ -0,0 +1,107 @@
+defmodule FullCircle.ReceiveFund.ReceiptDetail do
+  use FullCircle.Schema
+  import Ecto.Changeset
+  import FullCircle.Helpers
+
+  schema "receipt_details" do
+    field :descriptions, :string
+    field :discount, :decimal, default: 0
+    field :quantity, :decimal, default: 0
+    field :unit_price, :decimal, default: 0
+    field :tax_rate, :decimal, default: 0
+    field :package_qty, :decimal, default: 0
+    field :_persistent_id, :integer
+
+    belongs_to :receipt, FullCircle.ReceiveFund.Receipt
+    belongs_to :good, FullCircle.Product.Good
+    belongs_to :account, FullCircle.Accounting.Account
+    belongs_to :tax_code, FullCircle.Accounting.TaxCode
+    belongs_to :package, FullCircle.Product.Packaging
+
+    field :package_name, :string, virtual: true
+    field :account_name, :string, virtual: true
+    field :tax_code_name, :string, virtual: true
+    field :unit_multiplier, :decimal, virtual: true, default: 1
+    field :good_name, :string, virtual: true
+    field :unit, :string, virtual: true
+    field :amount, :decimal, virtual: true, default: 0
+    field :tax_amount, :decimal, virtual: true, default: 0
+    field :good_amount, :decimal, virtual: true, default: 0
+    field :delete, :boolean, virtual: true, default: false
+  end
+
+  def changeset(invoice_details, attrs) do
+    invoice_details
+    |> cast(attrs, [
+      :_persistent_id,
+      :quantity,
+      :unit_price,
+      :discount,
+      :descriptions,
+      :unit,
+      :account_name,
+      :tax_code_name,
+      :good_name,
+      :package_name,
+      :tax_rate,
+      :account_id,
+      :tax_code_id,
+      :good_id,
+      :package_id,
+      :package_qty,
+      :unit_multiplier,
+      :delete
+    ])
+    |> validate_required([
+      :quantity,
+      :unit_price,
+      :discount,
+      :tax_rate,
+      :package_name,
+      :account_name,
+      :tax_code_name,
+      :good_name
+    ])
+    |> validate_id(:good_name, :good_id)
+    |> validate_id(:tax_code_name, :tax_code_id)
+    |> validate_id(:package_name, :package_id)
+    |> validate_id(:account_name, :account_id)
+    |> compute_fields()
+    |> maybe_mark_for_deletion()
+  end
+
+  defp compute_fields(changeset) do
+    unit_multi = fetch_field!(changeset, :unit_multiplier)
+    pack_qty = fetch_field!(changeset, :package_qty)
+    price = fetch_field!(changeset, :unit_price)
+    disc = fetch_field!(changeset, :discount)
+    rate = fetch_field!(changeset, :tax_rate)
+
+    qty =
+      if Decimal.gt?(pack_qty, "0") and Decimal.gt?(unit_multi, "0") do
+        Decimal.mult(pack_qty, unit_multi)
+      else
+        fetch_field!(changeset, :quantity)
+      end
+
+    good_amount = Decimal.mult(qty, price) |> Decimal.add(disc) |> Decimal.round(2)
+    tax_amount = Decimal.mult(good_amount, rate) |> Decimal.round(2)
+    amount = Decimal.add(good_amount, tax_amount)
+
+    changeset
+    |> force_change(:good_amount, good_amount)
+    |> force_change(:tax_amount, tax_amount)
+    |> force_change(:amount, amount)
+    |> force_change(:quantity, qty)
+  end
+
+  defp maybe_mark_for_deletion(%{data: %{id: nil}} = changeset), do: changeset
+
+  defp maybe_mark_for_deletion(changeset) do
+    if get_change(changeset, :delete) do
+      %{changeset | action: :delete}
+    else
+      changeset
+    end
+  end
+end
--- priv/repo/migrations/20230714035051_create_receipts.exs
+++ priv/repo/migrations/20230714035051_create_receipts.exs
@@ -8,7 +8,7 @@ defmodule FullCircle.Repo.Migrations.CreateReceipts do
       add :contact_id, references(:contacts, on_delete: :restrict)
       add :descriptions, :text
       add :funds_account_id, references(:accounts, on_delete: :restrict)
-      add :receipt_amount, :decimal, default: 0
+      add :funds_amount, :decimal, default: 0
       add :company_id, references(:companies, on_delete: :delete_all)
 
       timestamps(type: :timestamptz)
@@ -18,6 +18,26 @@ defmodule FullCircle.Repo.Migrations.CreateReceipts do
     create index(:receipts, [:company_id])
     create index(:receipts, [:contact_id])
     create index(:receipts, [:funds_account_id])
+
+    create table(:receipt_details) do
+      add :_persistent_id, :integer
+      add :package_qty, :decimal, default: 0
+      add :package_id, references(:packagings, on_delete: :restrict)
+      add :quantity, :decimal, default: 0
+      add :unit_price, :decimal, default: 0
+      add :discount, :decimal, default: 0
+      add :tax_rate, :decimal, default: 0
+      add :descriptions, :string
+      add :receipt_id, references(:receipts, on_delete: :delete_all)
+      add :good_id, references(:goods, on_delete: :restrict)
+      add :account_id, references(:accounts, on_delete: :restrict)
+      add :tax_code_id, references(:tax_codes, on_delete: :restrict)
+    end
+
+    create index(:receipt_details, [:receipt_id])
+    create index(:receipt_details, [:good_id])
+    create index(:receipt_details, [:account_id])
+    create index(:receipt_details, [:tax_code_id])
 
     create table(:received_cheques) do
       add :_persistent_id, :integer
