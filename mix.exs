bd64ccb382e387b45b625e1833c0739923c936d0
Tan Kwang How <tankwanghow@gmail.com>
2024-06-17 15:40:16 +0800

    upgarde elixir 1.16.3 erlang 26.2.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FullCircle.MixProject do
     [
       app: :full_circle,
       version: "1.0.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.16.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [debug_info: Mix.env() == :dev],
       start_permanent: Mix.env() == :prod,
