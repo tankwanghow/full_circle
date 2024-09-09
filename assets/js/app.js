fc7ec32cf7b1d01517f5dd951e38da9ccfce74d3
Tan Kwang How <tankwanghow@gmail.com>
2024-09-09 13:49:00 +0800

    update not needed

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,6 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import { countPunchCard } from "./punch_card_count"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let Hooks = {}
