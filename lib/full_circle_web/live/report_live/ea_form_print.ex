3a22179428bfffb64f1959b78cc8288ac69a113a
Tan Kwang How <tankwanghow@gmail.com>
2025-03-10 11:42:51 +0800

    ea from v1

--- lib/full_circle_web/live/report_live/ea_form_print.ex
+++ lib/full_circle_web/live/report_live/ea_form_print.ex
@@ -75,31 +75,37 @@ defmodule FullCircleWeb.ReportLive.EAFormPrint do
     <div id="print-me" class="print-here">
       {style(assigns)}
       <div class="page">
-        <div class="field" style="top: 140px; left: 700px;">{@bosstaxno}</div>
-        <div class="field" style="top: 140px; left: 1045px;">{@year}</div>
-        <div class="field" style="top: 140px; left: 1200px;">{@branch}</div>
-        <div class="field" style="top: 200px; left: 950px;">{@name}</div>
-        <div class="field" style="top: 220px; left: 800px;">{@position}</div>
-        <div class="field" style="top: 125px; left: 1150px;">{@taxno}</div>
-        <div class="field" style="top: 238px; left: 800px;">{@ic}</div>
-        <div class="field" style="top: 256px; left: 800px;">{@epf_no}</div>
-        <div class="field" style="top: 285px; left: 850px;">{@children}</div>
-        <div class="field" style="top: 855px; left: 1220px;">{@childrended}</div>
-        <div class="field" style="top: 376px; left: 1220px;">{@income}</div>
-        <div :if={Decimal.to_float(@angpow) > 0} class="field" style="top: 413px; left: 1100px;">Ang Pow</div>
-        <div class="field" style="top: 413px; left: 1220px;">{@angpow}</div>
-        <div :if={Decimal.to_float(@bik) > 0} class="field" style="top: 542px; left: 1000px;">Kereta</div>
-        <div class="field" style="top: 542px; left: 1220px;">{@bik}</div>
-        <div class="field" style="top: 692px; left: 1220px; font-weight: bold;">{@total}</div>
-        <div :if={Decimal.to_float(@epf) > 0} class="field" style="top: 905px; left: 1000px;">KWSP</div>
-        <div class="field" style="top: 922px; left: 1220px;">{@epf}</div>
-        <div class="field" style="top: 735px; left: 1220px;">{@pcb}</div>
-        <div class="field" style="top: 940px; left: 1230px;">{@socso}</div>
-        <div class="field" style="top: 1088px; left: 680px;">{@gendate}</div>
-        <div class="field" style="top: 1012px; left: 1060px;">{@by}</div>
-        <div class="field" style="top: 1035px; left: 1060px;">{@bypos}</div>
-        <div class="field" style="top: 1053px; left: 1000px; width: 295px;">{@address}</div>
-        <div class="field" style="top: 1090px; left: 1090px;">{@phone}</div>
+        <div class="field" style="top: 75px; left: 150px;">{@bosstaxno}</div>
+        <div class="field" style="top: 58px; left: 495px;">{@year}</div>
+        <div class="field" style="top: 22px; left: 600px;">{@taxno}</div>
+        <div class="field" style="top: 20px; left: 650px;">{@branch}</div>
+        <div class="field" style="top: 58px; left: 400px;">{@name}</div>
+        <div class="field" style="top: 60px; left: 250px;">{@position}</div>
+        <div class="field" style="top: 59px; left: 250px;">{@ic}</div>
+        <div class="field" style="top: 59px; left: 250px;">{@epf_no}</div>
+        <div class="field" style="top: 73px; left: 300px;">{@children}</div>
+        <div class="field" style="top: 142px; left: 660px;">{@income}</div>
+        <div class="field" style="top: 159px; left: 550px;">
+          {if Decimal.to_float(@angpow) > 0, do: "Ang Pow", else: "-"}
+        </div>
+        <div class="field" style="top: 140px; left: 660px;">{@angpow}</div>
+        <div class="field" style="top: 250px; left: 450px;">
+          {if Decimal.to_float(@bik) > 0, do: "Kereta", else: "-"}
+        </div>
+        <div class="field" style="top: 230px; left: 660px;">{@bik}</div>
+        <div class="field" style="top: 362px; left: 660px; font-weight: bold;">{@total}</div>
+        <div class="field" style="top: 386px; left: 660px;">{@pcb}</div>
+        <div class="field" style="top: 488px; left: 670px;">{@childrended}</div>
+        <div class="field" style="top: 519px; left: 470px;">
+          {if Decimal.to_float(@epf) > 0, do: "KWSP", else: "-"}
+        </div>
+        <div class="field" style="top: 517px; left: 660px;">{@epf}</div>
+        <div class="field" style="top: 516px; left: 680px;">{@socso}</div>
+        <div class="field" style="top: 640px; left: 140px;">{@gendate}</div>
+        <div class="field" style="top: 550px; left: 500px;">{@by}</div>
+        <div class="field" style="top: 555px; left: 540px;">{@bypos}</div>
+        <div class="field" style="top: 553px; left: 470px; width: 295px;">{@address}</div>
+        <div class="field" style="top: 552px; left: 540px;">{@phone}</div>
       </div>
     </div>
     """
@@ -108,21 +114,19 @@ defmodule FullCircleWeb.ReportLive.EAFormPrint do
   defp style(assigns) do
     ~H"""
     <style>
-      .page { width: 210mm; min-height: 290mm; padding: 5mm;
+      .page { width: 210mm; height: 290mm; position: relative;
               background-image: url('/images/ea2023-1.jpg');
-              background-size: contain;
-              background-repeat: no-repeat;
-              background-position: center; }
+              background-size: 210mm 290mm;  }
 
-      .field {
-        position: absolute;
-      }
+      .field { position: relative; }
 
       @media print {
         @page { size: A4; margin: 0mm; }
-        body { width: 210mm; height: 290mm; margin: 0mm; }
-        html { margin: 0mm; }
-        .page { padding-left: 5mm; padding-right: 5mm; page-break-after: always;} }
+        .page { width: 210mm; height: 290mm; position: relative;
+              background-image: url('/images/ea2023-1.jpg');
+              background-size: 210mm 290mm;  }
+              .field { position: relative; }
+      }
     </style>
     """
   end
