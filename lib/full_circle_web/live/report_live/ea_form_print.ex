be072153884f0f99dc35cbfb8a6d2b7957323c86
Tan Kwang How <tankwanghow@gmail.com>
2025-03-15 16:37:34 +0800

    formatting

--- lib/full_circle_web/live/report_live/ea_form_print.ex
+++ lib/full_circle_web/live/report_live/ea_form_print.ex
@@ -124,122 +124,122 @@ defmodule FullCircleWeb.ReportLive.EAFormPrint do
     <div id="print-me" class="print-here">
       {style(assigns)}
       <div class="page">
-        <div class="field" style="top: 55px; left: 150px;">{@nosiri}</div>
-        <div class="field" style="top: 57px; left: 150px;">{@nomajikanE}</div>
-        <div class="field" style="top: 40px; left: 495px;">{@year}</div>
-        <div class="field" style="top: 3px; left: 580px;">{@tin}</div>
-        <div class="field" style="top: 2px; left: 650px;">{@lhdnbrh}</div>
-        <div class="field" style="top: 40px; left: 400px;">{@a1}</div>
-        <div class="field" style="top: 41px; left: 250px;">{@a2}</div>
-        <div class="field" style="top: 25px; left: 650px;">{@a3}</div>
-        <div class="field" style="top: 23px; left: 230px;">{@a4}</div>
-        <div class="field" style="top: 3px; left: 650px;">{@a5}</div>
-        <div class="field" style="top: 3px; left: 250px;">{@a6}</div>
-        <div class="field" style="top: -15px; left: 650px;">{@a7}</div>
-        <div class="field" style="top: -3px; left: 300px;">{zsd(@a8)}</div>
-        <div class="field" style="top: -18px; left: 650px;">{@a9a}</div>
-        <div class="field" style="top: -18px; left: 650px;">{@a9b}</div>
-        <div class="field" style="top: 27px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: 55px; left: 125px; text-align: center; width: 108px;">{@nosiri}</div>
+        <div class="field" style="top: 57px; left: 125px; text-align: center; width: 108px;">{@nomajikanE}</div>
+        <div class="field" style="top: 36px; left: 488px; text-align: center; width: 35px;">{@year}</div>
+        <div class="field" style="top: -2px; left: 550px; text-align: center; width: 190px;">{@tin}</div>
+        <div class="field" style="top: -5px; left: 635px; text-align: center; width: 110px;">{@lhdnbrh}</div>
+        <div class="field" style="top: 34px; left: 340px; text-align: center; width: 400px;">{@a1}</div>
+        <div class="field" style="top: 32px; left: 190px; text-align: center; width: 200px;">{@a2}</div>
+        <div class="field" style="top: 10px; left: 575px; text-align: center; width: 165px;">{@a3}</div>
+        <div class="field" style="top: 10px; left: 190px; text-align: center; width: 200px;">{@a4}</div>
+        <div class="field" style="top: -12px; left: 575px; text-align: center; width: 165px;">{@a5}</div>
+        <div class="field" style="top: -13px; left: 190px; text-align: center; width: 200px;">{@a6}</div>
+        <div class="field" style="top: -35px; left: 575px; text-align: center; width: 165px;">{@a7}</div>
+        <div class="field" style="top: -23px; left: 240px; text-align: center; width: 145px;">{zsd(@a8)}</div>
+        <div class="field" style="top: -40px; left: 575px; text-align: center; width: 165px;">{@a9a}</div>
+        <div class="field" style="top: -41px; left: 575px; text-align: center; width: 165px;">{@a9b}</div>
+        <div class="field" style="top: 4px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b1a, 2)}
         </div>
-        <div class="field" style="top: 27px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: 2px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b1b, 2)}
         </div>
-        <div class="field" style="top: 27px; left: 545px;">{@b1cname}</div>
-        <div class="field" style="top: 7px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -1px; left: 535px; text-align: center; width: 100px;">{@b1cname}</div>
+        <div class="field" style="top: -21px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b1c, 2)}
         </div>
-        <div class="field" style="top: 7px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -23px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b1d, 2)}
         </div>
-        <div class="field" style="top: 7px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -25px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b1e, 2)}
         </div>
-        <div class="field" style="top: 7px; left: 305px;">{@b1fdari}</div>
-        <div class="field" style="top: -12px; left: 475px;">{@b1fhingga}</div>
-        <div class="field" style="top: -32px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -26px; left: 255px; text-align: center; width: 140px;">{@b1fdari}</div>
+        <div class="field" style="top: -47px; left: 430px; text-align: center; width: 140px;">{@b1fhingga}</div>
+        <div class="field" style="top: -68px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b1f, 2)}
         </div>
 
-        <div class="field" style="top: -14px; left: 255px;">{@b2a}</div>
-        <div class="field" style="top: -14px; left: 255px;">{@b2b}</div>
-        <div class="field" style="top: -34px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -53px; left: 228px; text-align: center; width: 160px;">{@b2a}</div>
+        <div class="field" style="top: -54px; left: 228px; text-align: center; width: 160px;">{@b2b}</div>
+        <div class="field" style="top: -76px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b2, 2)}
         </div>
-        <div class="field" style="top: -35px; left: 365px;">{@b3nyata}</div>
-        <div class="field" style="top: -53px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -78px; left: 285px; text-align: center; width: 350px;">{@b3nyata}</div>
+        <div class="field" style="top: -98px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b3, 2)}
         </div>
 
-        <div class="field" style="top: -53px; left: 365px;">{@b4alamat}</div>
-        <div class="field" style="top: -73px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -100px; left: 258px; text-align: center; width: 378px;">{@b4alamat}</div>
+        <div class="field" style="top: -121px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b4, 2)}
         </div>
 
-        <div class="field" style="top: -73px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -123px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b5, 2)}
         </div>
-        <div class="field" style="top: -73px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -125px; left: 645px; text-align: right; width: 95px;">
           {zsd(@b6, 2)}
         </div>
 
-        <div class="field" style="top: -43px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -97px; left: 645px; text-align: right; width: 95px;">
           {zsd(@c1, 2)}
         </div>
-        <div class="field" style="top: -43px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -97px; left: 645px; text-align: right; width: 95px;">
           {zsd(@c2, 2)}
         </div>
         <div
           class="field"
-          style="font-weight: bold; top: -36px; left: 645px; text-align: right; width: 95px;"
+          style="font-weight: bold; top: -92px; left: 645px; text-align: right; width: 95px;"
         >
           {zsd(@jumlah, 2)}
         </div>
 
-        <div class="field" style="top: -13px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -69px; left: 645px; text-align: right; width: 95px;">
           {zsd(@d1, 2)}
         </div>
-        <div class="field" style="top: -13px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -71px; left: 645px; text-align: right; width: 95px;">
           {zsd(@d2, 2)}
         </div>
-        <div class="field" style="top: -13px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -74px; left: 645px; text-align: right; width: 95px;">
           {zsd(@d3, 2)}
         </div>
-        <div class="field" style="top: -15px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -76px; left: 645px; text-align: right; width: 95px;">
           {zsd(@d4, 2)}
         </div>
 
-        <div class="field" style="top: -1px; left: 525px; text-align: right; width: 95px;">
+        <div class="field" style="top: -65px; left: 475px; text-align: right; width: 160px;">
           {zsd(@d5a, 2)}
         </div>
 
-        <div class="field" style="top: -1px; left: 525px; text-align: right; width: 95px;">
+        <div class="field" style="top: -66px; left: 475px; text-align: right; width: 160px;">
           {zsd(@d5b, 2)}
         </div>
 
-        <div class="field" style="top: -3px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -70px; left: 645px; text-align: right; width: 95px;">
           {zsd(@d6, 2)}
         </div>
 
-        <div class="field" style="top: 26px; left: 445px;">
+        <div class="field" style="top: -41px; left: 225px; text-align: center; width: 510px;">
           {@e1name}
         </div>
 
-        <div class="field" style="top: 24px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -45px; left: 645px; text-align: right; width: 95px;">
           {zsd(@e1, 2)}
         </div>
-        <div class="field" style="top: 23px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="top: -47px; left: 645px; text-align: right; width: 95px;">
           {zsd(@e2, 2)}
         </div>
 
-        <div class="field" style="font-weight: bold; top: 35px; left: 645px; text-align: right; width: 95px;">
+        <div class="field" style="font-weight: bold; top: -37px; left: 645px; text-align: right; width: 95px;">
           {zsd(@f, 2)}
         </div>
-        <div class="field" style="top: 130px; left: 140px;">{@tarikh}</div>
-        <div class="field" style="top: 40px; left: 500px;">{@nama}</div>
-        <div class="field" style="top: 42px; left: 540px;">{@pos}</div>
-        <div class="field" style="top: 40px; left: 470px; width: 295px;">{@address}</div>
-        <div class="field" style="top: 38px; left: 540px;">{@phone}</div>
+        <div class="field" style="top: 60px; left: 100px; text-align: center; width: 145px;">{@tarikh}</div>
+        <div class="field" style="top: -35px; left: 440px; text-align: center; width: 295px;">{@nama}</div>
+        <div class="field" style="top: -35px; left: 440px; text-align: center; width: 295px;">{@pos}</div>
+        <div class="field" style="top: -37px; left: 440px; text-align: center; width: 295px;">{@address}</div>
+        <div class="field" style="top: -40px; left: 440px; text-align: center; width: 295px;">{@phone}</div>
       </div>
     </div>
     """
@@ -256,18 +256,18 @@ defmodule FullCircleWeb.ReportLive.EAFormPrint do
   defp style(assigns) do
     ~H"""
     <style>
-      .page { width: 210mm; height: 290mm; position: relative;
+      .page { width: 210mm; height: 297mm; position: relative;
               background-image: url('/images/ea2023-1.jpg');
-              background-size: 210mm 290mm;  }
+              background-size: 210mm 297mm;  }
 
-      .field { position: relative; }
+      .field { position: relative; border: 1px dotted white;}
 
       @media print {
         @page { size: A4; margin: 0mm; }
-        .page { width: 210mm; height: 290mm; position: relative;
+        .page { width: 210mm; height: 297mm; position: relative;
               background-image: url('/images/ea2023-1.jpg');
-              background-size: 210mm 290mm;  }
-              .field { position: relative; }
+              background-size: 210mm 297mm;  }
+        .field { position: relative; }
       }
     </style>
     """
