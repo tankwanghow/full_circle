221a95d5648bca904164b25e06bc15d7edf7849d
Tan Kwang How <tankwanghow@gmail.com>
2026-03-10 14:54:14 +0800

    update deploy steps

--- deploy_to_linode/deploy.sh
+++ deploy_to_linode/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 # Path to your setup file
 SETUP_FILE=$1
@@ -18,7 +18,7 @@ do
     # Ignore comments and empty lines
     [[ "$key" =~ ^#.*$ ]] && continue
     [[ "$key" =~ ^[[:space:]]*$ ]] && continue
-    
+
     # Remove leading and trailing whitespace from key and value
     key=$(echo $key | tr -d '[:space:]')
     value=$(echo $value | tr -d '[:space:]')
@@ -35,13 +35,16 @@ echo
 
 # Configuration
 IMAGE_TAG="latest"
+FULL_IMAGE="$DOCKER_HUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
 
 # Build Docker image
 echo "Building Docker image... $script_path"
-docker build -t $DOCKER_HUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG -f $script_path/../Dockerfile $script_path/..
+docker build -t $FULL_IMAGE -f $script_path/../Dockerfile $script_path/..
 
-# Push the Docker image to Docker Hub
-echo "Pushing image to Docker Hub..."
-docker push $DOCKER_HUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+# Transfer image directly to server (skip Docker Hub)
+IMAGE_SIZE=$(docker image inspect $FULL_IMAGE --format='{{.Size}}')
+echo "Transferring image to server (~$(( IMAGE_SIZE / 1024 / 1024 )) MB uncompressed)..."
+docker save $FULL_IMAGE | gzip | pv | sshpass -p $LINODE_PWD ssh -o StrictHostKeyChecking=no root@$LINODE_IP "gunzip | docker load"
 
-sshpass -p $LINODE_PWD ssh root@$LINODE_IP  "bash /home/$IMAGE_NAME/deploy_at_server.sh $IMAGE_NAME $DOCKER_HUB_USERNAME $DOCKER_CONTAINER_NAME"
+# Restart containers and run migrations
+sshpass -p $LINODE_PWD ssh root@$LINODE_IP "bash /home/$IMAGE_NAME/deploy_at_server.sh $IMAGE_NAME $DOCKER_HUB_USERNAME $DOCKER_CONTAINER_NAME"
--- deploy_to_linode/deploy_at_server.sh
+++ deploy_to_linode/deploy_at_server.sh
@@ -7,10 +7,8 @@ DOCKER_HUB_USERNAME=$2
 DOCKER_CONTAINER_NAME=$3
 APP_COMPOSE="/home/$IMAGE_NAME/docker-compose-$IMAGE_NAME.yml"
 
-# SSH into Linode and pull & run the Docker image
-echo "Instructing Linode to pull and run the new image..."
+# Restart container with the new image
 echo "Updating container on Linode..."
-docker pull $DOCKER_HUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
 docker compose -f $APP_COMPOSE down
 docker compose -f $APP_COMPOSE up -d
 echo "Running migration for $DOCKER_CONTAINER_NAME...."
